d00345012f93827f4b186ad422f8540b4349ba1e
Duncan MacLennan <dcupmusic@gmail.com>
2023-12-21 09:26:43 +1100

    fix pep8

--- api/v1/app.py
+++ api/v1/app.py
@@ -12,6 +12,7 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def teardown_appcontext(exception):
     storage.close()
@@ -20,4 +21,3 @@ if __name__ == "__main__":
     host = os.environ.get('HBNB_API_HOST', '0.0.0.0')
     port = int(os.environ.get('HBNB_API_PORT', 5000))
     app.run(host=host, port=port, threaded=True)
-
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,6 +6,7 @@ Runs the index.py file
 from flask import jsonify
 from api.v1.views import app_views
 
+
 @app_views.route('/status', methods=['GET'])
 def get_status():
     return jsonify({"status": "OK"})
