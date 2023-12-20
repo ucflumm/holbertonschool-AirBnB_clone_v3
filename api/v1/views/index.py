0c4d1e7aa3e0ff67204cea3dbd7117909867ea7c
Duncan MacLennan <dcupmusic@gmail.com>
2023-12-20 17:47:03 +1100

    first commit .. starter boilerplate

---
+++ api/v1/views/index.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+from flask import jsonify
+from api.v1.views import app_views
+
+
+@app_views.route('/status', methods=['GET'])
+def get_status():
+    return jsonify({"status": "OK"})
