0de6c991f22af2573abc7a1286acc9e6de7b8fe4
jess6718 <6718@holbertonstudents.com>
2023-12-20 12:14:49 +0100

    feat: add task 5

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,8 +1,27 @@
 #!/usr/bin/env python3
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route('/status', methods=['GET'])
 def get_status():
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats')
+def get_counts():
+    return jsonify({
+        'amenities': storage.count(Amenity),
+        'cities': storage.count(City),
+        'places': storage.count(Place),
+        'reviews': storage.count(Review),
+        'states': storage.count(State),
+        'users': storage.count(User)
+        })
