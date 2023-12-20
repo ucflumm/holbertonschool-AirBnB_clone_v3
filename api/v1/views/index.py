124f8f78afa56c1cc8699f8eca3fca155ddb54c6
Duncan MacLennan <dcupmusic@gmail.com>
2023-12-21 09:24:15 +1100

    added docs to index

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+"""
+Runs the index.py file
+"""
+
 from flask import jsonify
 from api.v1.views import app_views
-
 
 @app_views.route('/status', methods=['GET'])
 def get_status():
