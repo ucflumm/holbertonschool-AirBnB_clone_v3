fc8ee95a88847ee52197f041ecd39088e83812d7
jess6718 <6718@holbertonstudents.com>
2023-12-21 00:20:26 +0100

    handle error

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,6 +5,7 @@ Runs the app.py file
 
 
 from flask import Flask
+from flask import jsonify
 from models import storage
 from api.v1.views import app_views
 import os
@@ -16,6 +17,12 @@ app.register_blueprint(app_views)
 @app.teardown_appcontext
 def teardown_appcontext(exception):
     storage.close()
+
+
+@app.errorhandler(404)
+def handle_error(e):
+    return jsonify({"error": "Not found"}), 404
+
 
 if __name__ == "__main__":
     host = os.environ.get('HBNB_API_HOST', '0.0.0.0')
